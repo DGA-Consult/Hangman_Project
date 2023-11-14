b5238b943aafce8aac183f19e28f9179e781a2c7
Dmitri Gott <dmitri.vitalyevich.gott@googlemail.com>
2023-11-14 20:57:08 +0000

    optimising code for milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -3,22 +3,19 @@ import random
 # Creating a list of fruit names
 word_list = ['Apple', 'Banana', 'Orange', 'Grapes', 'Mango']
 
-# Printing the list
-print(word_list)
-
-# Using random.choice to select a random fruit
+# Selecting a random word from the list
 word = random.choice(word_list)
 
-# Printing the random word
-print(word)
+# Displaying the list and the randomly chosen word
+print("Word List:", word_list)
+print("Random Word:", word)
 
-# Using the input function to get a single letter from the user
+# Getting a single letter guess from the user
 guess = input("Enter a single letter: ")
 
-# Checking if the input is a single alphabetical character
+# Checking if the input is a valid guess
 if len(guess) == 1 and guess.isalpha():
-    # If the conditions are met, print "Good guess!"
     print("Good guess!")
 else:
-    # If the conditions are not met, print "Oops! That is not a valid input."
     print("Oops! That is not a valid input.")
+
